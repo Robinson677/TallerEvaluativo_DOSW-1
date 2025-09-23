850453bb1eb3f413e88cdf1185ebe4d657c61d7b
Oscar Sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-09-23 17:12:48 -0500

    Estructurado de carpetas_OscarSanchez

---
+++ src/main/java/edu/dosw/TallerEvaluativo/TallerEvaluativo_DOSW_1/TallerEvaluativoDosw1Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.TallerEvaluativo.TallerEvaluativo_DOSW_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerEvaluativoDosw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerEvaluativoDosw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/TallerEvaluativo/TallerEvaluativo_DOSW_1/TallerEvaluativoDosw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.TallerEvaluativo.TallerEvaluativo_DOSW_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerEvaluativoDosw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
